73f1f987a1527a2dece6db4383520104d9eff7ac
Richard Seldon <arcseldon@gmail.com>
2016-03-02 05:39:46 +0900

    quick update

--- rules/empty.js
+++ rules/empty.js
@@ -1,5 +1,5 @@
 function (user, context, callback) {
-  // TODO: implement your rule
+  // TODO: implement your rules
   console.log('does this work?');
   console.log(configuration.name);
   callback(null, user, context);
